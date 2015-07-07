ee0ed5f3c239ecdc53808f4ca2c41c647e006c90
Leonardo Brescia <leonardo@made4u.com.br>
2015-07-07 14:39:29 -0300

    Formatação final do arquivo page.php Arquivo padrao para criação de paginas no WP

--- wp-content/themes/gestiona/page.php
+++ wp-content/themes/gestiona/page.php
@@ -1,3 +1,23 @@
 <?php get_header(); ?>
+<?php global $post;?>
+<div id="banner-interno" class="container-fluid <?php echo $post->post_name;?>">
+	<div class="container text-center">
+    	<h1><?php echo get_the_title(); ?></h1>
+    </div>
+</div><!--banner-interno-->
 
+<section id="page" class="container-fluid">
+    <div class="container">
+        <div class="row">	
+             <?php if( have_posts() ) : while( have_posts() ) : the_post() ?>
+              
+                <?php the_content() ?>
+            <?php endwhile; ?>
+              
+            <?php else : ?>
+                <p></p>
+            <?php endif ?>
+        </div><!--row-->
+    </div><!--container-->
+</section>
 <?php get_footer(); ?>
