108a698c369fb6b98b003d1dc44a08c0f0184f59
Leonardo Brescia <leonardo@made4u.com.br>
2015-07-09 13:44:59 -0300

    Tamplate de barra lateral

---
+++ wp-content/themes/gestiona/sidebar.php
@@ -0,0 +1,8 @@
+<aside class="col-xs-3 col-xs-offset-1 visible-md visible-lg">
+	<h4>Gestiona nas redes:</h4>
+    <div id="widget-list">
+
+       <?php dynamic_sidebar( 'Barra Lateral' ); ?>
+
+    </div>
+</aside>
