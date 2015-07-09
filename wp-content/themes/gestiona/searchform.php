700f55250b9b62784e58f4eab52944ef0859ebcb
Leonardo Brescia <leonardo@made4u.com.br>
2015-07-09 13:45:20 -0300

    Tamplate de formulario de busca

---
+++ wp-content/themes/gestiona/searchform.php
@@ -0,0 +1,10 @@
+<?php
+/*
+* The template for displaying Search Form
+* Template Name: Search Form
+*/
+?>
+<form id="searchform" action="/" method="get" accept-charset="utf-8" role="search">
+    <div><input type="text" name="s" id="s" value="<?php the_search_query(); ?>" placeholder="Pesquise por..." >
+    <input type="submit" id="searchsubmit" value=" " ></div>
+</form>
